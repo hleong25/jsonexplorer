3bd26fbf9b23ab459783b542d4d914ae18457df5
henry leong <hleong25+github@gmail.com>
2017-01-22 11:26:54 -0800

    ng generate component json-viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { JsonViewerComponent } from './json-viewer/json-viewer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JsonViewerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/json-viewer/json-viewer.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { JsonViewerComponent } from './json-viewer.component';
+
+describe('JsonViewerComponent', () => {
+  let component: JsonViewerComponent;
+  let fixture: ComponentFixture<JsonViewerComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JsonViewerComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JsonViewerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
